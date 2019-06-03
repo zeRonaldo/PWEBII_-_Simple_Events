746dce15c3a0fb1a225456fb7a5b17af2f1e36aa
@ZeRonaldo <joseronaldosjr@gmail.com>
2019-06-03 19:09:34 -0300

    initial Commit: default spring initialized

---
+++ src/main/java/br/edu/ifpb/simpleevents/SimpleEventsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.simpleevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEventsApplication.class, args);
+	}
+
+}
